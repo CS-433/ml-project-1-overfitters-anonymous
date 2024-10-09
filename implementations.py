83dd39cd47227aa7c4a42d2127867c4f837396e9
Max Tost <m.luke.tost@gmail.com>
2024-10-09 15:44:09 +0200

    Added function head of logistic_regression(y, tx, w)

--- implementations.py
+++ implementations.py
@@ -1,1 +1,38 @@
 # Contains all functions needed for project 1
+
+
+
+def logistic_regression(y, tx, w):
+    """return the loss, gradient of the loss, and hessian of the loss.
+
+    Args:
+        y:  shape=(N, 1)
+        tx: shape=(N, D)
+        w:  shape=(D, 1)
+
+    Returns:
+        loss: scalar number
+        gradient: shape=(D, 1)
+        hessian: shape=(D, D)
+
+    >>> y = np.c_[[0., 1.]]
+    >>> tx = np.arange(6).reshape(2, 3)
+    >>> w = np.array([[0.1], [0.2], [0.3]])
+    >>> loss, gradient, hessian = logistic_regression(y, tx, w)
+    >>> round(loss, 8)
+    0.62137268
+    >>> gradient, hessian
+    (array([[-0.10370763],
+           [ 0.2067104 ],
+           [ 0.51712843]]), array([[0.28961235, 0.3861498 , 0.48268724],
+           [0.3861498 , 0.62182124, 0.85749269],
+           [0.48268724, 0.85749269, 1.23229813]]))
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # return loss, gradient, and Hessian: TODO
+    # ***************************************************
+    raise NotImplementedError
+
+
+test(logistic_regression)
