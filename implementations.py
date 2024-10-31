c602d647b1cb65dcc485d07c6cf45c4e4f20c63e
Jérémy Salm <130314261+jeremslm@users.noreply.github.com>
2024-10-31 23:35:51 +0100

    Update implementations.py

--- implementations.py
+++ implementations.py
@@ -434,7 +434,7 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
             break
     
-    print("Lambda={lam}, Training loss={l}".format(lam=lambda_, l=loss))
+    #print("Lambda={lam}, Training loss={l}".format(lam=lambda_, l=loss))
     return w.ravel(), loss
 
 
